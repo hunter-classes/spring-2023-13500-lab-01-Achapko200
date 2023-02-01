3f6b7f52ade2d1f9b85602c69e66fc5f5df88572
Achapko200 <111392651+Achapko200@users.noreply.github.com>
2023-02-01 15:43:22 -0500

    Add files via upload

---
+++ labs/month.cpp
@@ -0,0 +1,57 @@
+/*
+spring 2023 - Lab1C
+Chapko
+Anna 
+Achapko200
+
+Description: the prompt asks the user to imput the month and the year then it counds how many days are in there 
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+  int year;
+  int month;
+  cout<< "enter year";
+  cin>> year;
+  cout<< "enter month";
+  cin>> month;
+  if (year % 4 !=0){
+      if (month == 2){  
+    cout<<"28 days";
+  } else if (month % 2 ==0 && month <=7){
+    cout<< "30 days";
+  } else if (month>7 && month % 2 ==1){
+    cout<< "30 days";
+  } else {
+    cout<< "31 days";
+  }
+  }else if (year % 100 !=0){
+    if (month ==2){
+      cout<<"29 days";
+    } else if (month ==1||month==3||month==5||month==7||month==8||month==10||month==12){
+      cout<<"31 days";
+    } else{
+      cout<<"30 days";
+    }
+  } else if(year % 400 !=0){
+    if(month==2){
+      cout<<"28 days";
+    } else if (month % 2 ==0 && month<=7){
+      cout<<"30 days";
+    } else if (month >7 && month % 2 ==1){
+      cout<<"30 days";
+    } else{
+      cout<< "31 days";
+    }
+  } else {
+    if (month==2){
+      cout<<"29 days";
+    } else if (month==1||month==3||month==5||month==7||month==8||month==10||month==12){
+      cout<<"31 days";
+    } else {
+      cout<<"30 days";
+    }
+  }
+  return 0; 
+}
